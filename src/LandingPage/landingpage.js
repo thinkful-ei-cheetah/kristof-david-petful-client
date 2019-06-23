d17f1951c2119dbe69480e60c95a7ec6ff6d1cae
Kristof-Pierre Cummings <kristofpierre8@gmail.com>
2019-06-22 23:32:38 -0500

    added css to title

--- src/LandingPage/landingpage.js
+++ src/LandingPage/landingpage.js
@@ -1,12 +1,12 @@
 import React from 'react';
-
+import './landingpage.css'
 
 const LandingPage = ({history}) => {
 
   return (
   <div>
-    <h1>Welcome to Petful</h1>
-    <p>You can join a queue to adopt a pet. Keep in mind that after adopting a dog or a cat, you will need to rejoin the queue to adopt an animal of the same type. Of course, you can still adopt the other animal whil you wait.
+    <h1 className="landing-welcome">Welcome to Petful</h1>
+    <p className="landing-text">You can join a queue to adopt a pet. Keep in mind that after adopting a dog or a cat, you will need to rejoin the queue to adopt an animal of the same type. Of course, you can still adopt the other animal whil you wait.
     </p>
     <input type="button" value="Let's Go" onClick={() => {history.push('/adopt')}}/>
   </div>
